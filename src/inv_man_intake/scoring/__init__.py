7758e40815e379b21d251a754d08e9f8fe22c149
Tim Stranske <stranske@gmail.com>
2026-03-08 03:11:05 -0500

    Issue #38: add scoring explainability payload contract

--- src/inv_man_intake/scoring/__init__.py
+++ src/inv_man_intake/scoring/__init__.py
@@ -11,6 +11,13 @@ from inv_man_intake.scoring.engine import (
     compute_score,
     default_weights_by_asset_class,
 )
+from inv_man_intake.scoring.explainability import (
+    ExplainabilityPayload,
+    ScoreComponentInput,
+    ScoreComponentOutput,
+    build_explainability_payload,
+    format_explainability_payload,
+)
 from inv_man_intake.scoring.regression import (
     CalibrationStats,
     DriftAlert,
@@ -25,15 +32,20 @@ __all__ = [
     "CalibrationStats",
     "DriftAlert",
     "DriftReport",
+    "ExplainabilityPayload",
     "RedFlagDecision",
     "RedFlagHook",
     "ScoreComponent",
+    "ScoreComponentInput",
+    "ScoreComponentOutput",
     "ScoreEntry",
     "ScoreResult",
     "ScoreSubmission",
+    "build_explainability_payload",
     "build_calibration_stats",
     "compute_score",
     "default_weights_by_asset_class",
     "detect_score_drift",
+    "format_explainability_payload",
     "rank_by_asset_class",
 ]
